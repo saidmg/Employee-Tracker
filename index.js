c5a245ac7a5ac96f9e44c2d5b14b23228d89ff21
Said Mg <saidmghabghab@gmail.com>
2021-02-21 19:57:09 -0500

    Completed the view details choice

--- index.js
+++ index.js
@@ -2,24 +2,55 @@ const inquirer = require( 'inquirer' );
 // module to connect to database
 const db = require( './app/connection' )('content_management_systems','skotkalb')
 
- async function main(){
+ async function InitialQuestion(){
     // get new song info
-    const firstQuestion =  inquirer.prompt([
-        { name: "firstChoice",    message: "What would you like to do", type: "list", choices:["View All Employees","Update Employees",] },
+     await inquirer.prompt([
+        { name: "firstChoice",    message: "What would you like to do", type: "list", choices:["View Details","Update/Add/Delete",] },
     ])
-
-    const response = await promptUser();  
-    if (response.firstChoice === "View All Employees"){
-        const myResult = await db.query( "SELECT * FROM Employees",);
-        console.log( `insert result:`, myResult )
+ }
+ 
+    const response = await InitialQuestion();  {
+    if (response.firstChoice === "View Details"){
+        viewDetails()
     }
     else{
-        const updateEmployees =  inquirer.prompt([
-            { name: "firstChoice",    message: "In Which department?", type: "list", choices:["Sales","Engineering","Finance","Legal",] },
+        const updateEmployees = await inquirer.prompt([
+            { name: "firstName",    message: "What is the employee's first name",type: "input"},
+            { name: "lasttName",    message: "What is the employee's last name", type: "input" },
+            { name: "role",    message: "What is the employee's role", type: "list", choices:["Sales Lead","Salesperson","Lead Engineer",
+            "Software Engineer", "Account Manager", "Accountant",  "Legal Team Lead",] },
+            { name: "managerName",    message: "In Which department?", type: "list", choices:["Sales","Engineering","Finance","Legal",]},
         ])
     }
+    }
+    async function viewDetails (){
 
- }
+        await inquirer.prompt([
+            { name: "viewDetails",    message: "What would you like to view", type: "list", 
+            choices:["Employees","Departments","Roles","total utilized budget of a department"] },
+        ])
+        // const myResult = await db.query( "SELECT * FROM Employees",);
+        // console.log( `insert result:`, myResult )
+    }
+    const response2 = await viewDetails();  
+    if (response2.viewDetails === "Employees"){
+        //  viewEmployees()
+        const myResult = await db.query("SELECT * FROM employees")
+        console.log(myResult)
+    }
+    else if(response2.viewDetails === "Departments"){
+        const myResult = await db.query("SELECT * FROM department")
+        console.log(myResult)
+    }
+     else if(response2.viewDetails === "Roles"){
+        const myResult = await db.query("SELECT * FROM role")
+        console.log(myResult)
+    }
+    else if(response2.viewDetails === "total utilized budget of a department"){
+        const myResult = await db.query("SELECT SUM(salary) FROM role;")
+        console.log(myResult)
+    }
+    
     // const myResult = await db.query( 
     //     "INSERT INTO favourite_songs (title,artist,genre,rating) VALUES(?,?,?,?) ",
     //     [newSong.title, newSong.artist, newSong.genre, newSong.rating] );
